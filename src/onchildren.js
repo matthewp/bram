3921bac39f8b4e21988ee3e33eafed79c9393779
Matthew Phillips <matthew@matthewphillips.info>
2016-08-28 10:32:17 -0400

    Bram.onChildren
    This adds the Bram.onChildren function. This is useful to set up a
    callback that is called whenever the element's children change (whether
        added or removed). This is for the case where you have a custom
    element that knows about its children and does something like sorts
    them, the onChildren callback is called whenever those children change.

---
+++ src/onchildren.js
@@ -0,0 +1,20 @@
+Bram.onChildren = function(element, callback){
+  var cancelled = false;
+  var report = function(){
+    if(!cancelled) {
+      callback(element.childNodes);
+    }
+  };
+
+  var mo = new MutationObserver(report);
+  mo.observe(element, { childList: true });
+
+  if(element.childNodes.length) {
+    Promise.resolve().then(report);
+  }
+
+  return function(){
+    cancelled = true;
+    mo.disconnect();
+  };
+};
