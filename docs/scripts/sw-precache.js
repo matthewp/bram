c18f0d498d539673a10528efcbdae3b25bac833d
Matthew Phillips <matthew@matthewphillips.info>
2016-11-28 07:18:08 -0500

    Add some missing service worker files

--- docs/scripts/sw-precache.js
+++ docs/scripts/sw-precache.js
@@ -3,12 +3,16 @@ const rootDir = 'docs';
 
 swPrecache.write(__dirname + '/../service-worker.js', {
   staticFileGlobs: [
+    `${rootDir}/`,
     `${rootDir}/styles.css`,
+    `${rootDir}/index.css`,
+    `${rootDir}/styles/hljs/atom-one-dark.css`,
     `${rootDir}/bram.umd.js`,
     `${rootDir}/examples/tabs/tabs.js`,
     `${rootDir}/examples/todos/app.js`,
     `${rootDir}/examples/pull-requests/prs.js`,
-    `${rootDir}/scripts/highlight.pack.js`
+    `${rootDir}/scripts/highlight.pack.js`,
+    `${rootDir}/images/bram.svg`
   ],
   stripPrefix: rootDir
 });
